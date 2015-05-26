9bb595e90e94e9adc03b0816acf018900c691520
anthonyrgreen <argreen@umich.edu>
2015-05-26 12:29:55 -0400

    adding first data-viewing script stuff

--- records/static/viewModuleLogs.js
+++ records/static/viewModuleLogs.js
@@ -1,42 +1,64 @@
-$(function () {
-  /*$("#outputType").submit(function (event) {
-    event.preventDefault();
-    formdata = $("#outputType").serializeArray();
-    if(formdata.length != 0)
-      outputType = formdata[0]["value"];
-    if(!outputType) {
-      console.log("no input");
-    }
-    else {
-      switch (outputType) {
-        case 'date':
-          console.log("date func");
-          $(this).append(timeForm);
-          break;
-        case 'user':
-          console.log("user func");
-          break;
-        case 'package':
-          console.log("package func");
-          break;
-        default:
-          console.log("error func");
-          break;
-      }
-    }
-    event.preventDefault();
-  });*/
+/*$(function () {
+  // Grey out and deselect version when package is unchecked
   $("input[value='package']").change( function () {
-    console.log("fired.");
     if ($(this)[0].checked) {
       $("input[value='version']").prop("disabled", false);
-      console.log("should be enabled");
     }
     else {
-      console.log("should be disabled");
       $("input[value='version']").prop("checked", false);
       $("input[value='version']").prop("disabled", true);
     }
   });
-  
-});
+});*/
+
+dummyData = [ { 'count' : 123, 'module' : 'R', 'user' : 'wellman'}
+            , { 'count' : 100, 'module' : 'R', 'user' : 'carmenal'}
+            , { 'count' : 50, 'module' : 'gcc', 'user' : 'bergis'}
+            , { 'count' : 30, 'module' : 'gcc', 'user' : 'wellman'} ]
+dummyOpts = ['module', 'user']
+
+buildDataTree = function (aggregationOrdering, info) {
+// ex: aggregationOrdering = ['module', 'user']
+// dict = [(123, 'R', 'wellman'), (100, 'R', 'carmenal'),
+//         (50, 'gcc', 'bergis'), (30, 'gcc', 'wellman')]
+// -> { 'R'   : { 'wellman' : 123
+//              , 'carmenal' : 100 }
+//    , 'gcc' : { 'bergis' : 50 
+//              , 'wellman' : 30 } }
+// TODO: add ascending/descending option
+  /*var idxDict = { 'count'     : 0
+                , 'module'    : 1
+                , 'version'   : 3
+                , 'username'  : 2 }*/ // TODO: figure out how indexing works
+                                    // (send dicts from server?)
+
+  // add inserElt to dict in-place, branching down aggregationOrdering's
+  // opts in a tree-like fashion
+  var addToDict = function (dict, insertElt) {
+    var localDict = dict
+
+    for (var i = 0; i < aggregationOrdering.length; i++) {
+      var insertKey = insertElt[aggregationOrdering[i]]
+      if (i < aggregationOrdering.length - 1) {
+        if (localDict[insertKey] === undefined)
+          localDict[insertKey] = {}
+        localDict = localDict[insertKey]
+      }
+      else
+        localDict[insertKey] = insertElt['count']
+    }
+
+    return dict
+  }
+
+  return foldl (addToDict, {}, info);
+}
+
+foldl = function (fun, acc, list) {
+  for (var i = 0; i < list.length; i++)
+    acc = fun(acc, list[i])
+  return acc
+}
+buildDataGraph = function (dataTree, aggregationOrdering) {
+
+}
