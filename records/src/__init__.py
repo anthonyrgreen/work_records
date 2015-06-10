7617162c042e76790d256217c6296bf2364ba344
anthonyrgreen <argreen@umich.edu>
2015-06-10 15:25:46 -0400

    small fixes

--- records/src/__init__.py
+++ records/src/__init__.py
@@ -4,10 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from records.config import basedir, SQLALCHEMY_DATABASE_URI
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
-Base = declarative_base(bind=engine)
 Session = sessionmaker(bind=engine)
-
-from records.src.dbCreate import createAll
+Base = declarative_base(bind=engine)
 from records.src.schema import ModuleLoadRecord, LogFile
-
-createAll()
+Base.metadata.create_all()
--- records/src/dbCreate.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy_utils.functions import create_database
-from records.config import SQLALCHEMY_DATABASE_URI
-from records.src import Base
-
-def createAll():
-  #create_database(SQLALCHEMY_DATABASE_URI)
-  Base.metadata.create_all()
