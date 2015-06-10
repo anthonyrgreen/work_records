cc9b1e4398ff9fec3b4c0bd4df7dc5553a5db2bd
Anthony Green <argreen@umich.edu>
2015-06-10 16:29:40 -0400

    Made one more wrapper dir.

--- records/deleteDirLogs.py
+++ records/deleteDirLogs.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 from sys import argv, path
-path.insert(0,'..')
-path.insert(0,'../..')
 from appContext import dbMaintenance
-from records.models.maintenance import deleteModuleLogDirectory
+from records.src.maintenance import deleteModuleLogDirectory
 
 def deleteLogs():
   for arg in argv[1:]:
